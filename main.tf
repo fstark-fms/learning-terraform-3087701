0adbd806945373819ea4953a491a23bfd43ddb2a
Francois Stark <98029549+fstark-fms@users.noreply.github.com>
2024-02-21 15:57:12 +0200

    changed SG name

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_vpc" "default" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog_new"
+  name = "blog"
   
   vpc_id = data.aws_vpc.default.id
 
