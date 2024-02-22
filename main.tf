d1cc3ba4cb5694213f45c9047b39a19d3bd6b7c9
Francois Stark <98029549+fstark-fms@users.noreply.github.com>
2024-02-22 16:57:13 +0200

    added autoscaling

--- main.tf
+++ main.tf
@@ -43,17 +43,21 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source   = "terraform-aws-modules/autoscaling/aws"
+  version  = "7.4.0"
+
+  name     = "blog-autoscale"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
 module "alb" {
