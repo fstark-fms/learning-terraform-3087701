355dd334973b1bf7d7bd9e300cba7a5338337b70
Francois Stark <98029549+fstark-fms@users.noreply.github.com>
2024-02-26 14:45:35 +0200

    refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,28 +11,28 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] 
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "fs_dev_vpc"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${environment.network_prefix}.101.0/24", "${environment.network_prefix}.102.0/24", "${environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "Development"
+    Environment = var.environment.name
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog_new"
+  name = "${var.environment.name}_blog_new"
   
   vpc_id = module.blog_vpc.vpc_id
 
@@ -47,9 +47,9 @@ module "autoscaling" {
   source   = "terraform-aws-modules/autoscaling/aws"
   version  = "7.4.0"
 
-  name     = "blog-autoscale"
-  min_size = 1
-  max_size = 2
+  name     = "${var.environment.name}-blog-autoscale"
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
@@ -64,7 +64,7 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name    = "blog-alb"
+  name    = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -82,7 +82,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -90,7 +90,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "Development"
+    Environment = var.environment.name
     Project     = "Blog_FS"
   }
 }
