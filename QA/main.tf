51db83041db3ef7780030e3ad08bbd7cf13afde9
Francois Stark <fstark@fmsystems.com>
2024-03-13 12:19:43 +0200

    added QA anvironment

---
+++ QA/main.tf
@@ -0,0 +1,13 @@
+module "QA" {
+    source = "../modules/blog"
+
+    environment = {
+        name = "QA"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
+
+    
+}
