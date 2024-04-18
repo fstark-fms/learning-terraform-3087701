af878b76753661eaab7e265fb7a5f2da3b3e769e
Francois Stark <98029549+fstark-fms@users.noreply.github.com>
2024-04-18 16:37:09 +0200

    gix autoscaling arns

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -2,6 +2,15 @@ output "environment_alb_url" {
     value = module.blog_alb.dns_name
 }
 
+output "environment_alb_target_groups" {
+    value = module.blog_alb.target_groups
+}
+
+output "environment_alb_target_group_arns" {
+    value = module.blog_alb.target_group_arns
+}
+
+
 output "environment_url" {
     value = module.blog_acm.distinct_domain_names
 }
