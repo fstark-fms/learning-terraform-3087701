92872dd421efbe905a0836395f0bb9f707929bd6
Francois Stark <98029549+fstark-fms@users.noreply.github.com>
2024-04-18 16:38:46 +0200

    reduced outputs

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -6,11 +6,6 @@ output "environment_alb_target_groups" {
     value = module.blog_alb.target_groups
 }
 
-output "environment_alb_target_group_arns" {
-    value = module.blog_alb.target_group_arns
-}
-
-
 output "environment_url" {
     value = module.blog_acm.distinct_domain_names
 }
