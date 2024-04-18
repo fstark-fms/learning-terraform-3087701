42ae183dba723c038fa4c3acb850e6332f7a540c
Francois Stark <98029549+fstark-fms@users.noreply.github.com>
2024-04-18 14:15:19 +0200

    fmix alb outpus 3

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -1,5 +1,11 @@
-output "environment_url" {
+output "environment_alb_url" {
     value = module.blog_alb.lb_dns_name
-    env_url = module.blog_alb.environment_url
-    url = module.blog_alb.url
+}
+
+output "environment_url" {
+    value = module.blog_alb.environment_url
+}
+
+output "url" {
+    value = module.blog_alb.url
 }
