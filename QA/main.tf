ddcded0f06057460efc0f318b81448988c2f7aa9
Francois Stark <98029549+fstark-fms@users.noreply.github.com>
2024-04-19 14:06:39 +0200

    added qa zone id

--- QA/main.tf
+++ QA/main.tf
@@ -5,6 +5,7 @@ module "QA" {
         name = "QA"
         network_prefix = "10.1"
         cert_subdomain = "blog-qa"
+        zone_id = "Z05530962ORLJ19VPIYSO"        
     }
 
     asg_min_size = 1
